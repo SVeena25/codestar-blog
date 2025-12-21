5c7ef7c798919954d96a44565e93b9198d5813e2
Veena S <veenasriramoju@gmail.com>
2025-12-21 18:26:25 +0000

    Refactor .gitignore to remove unnecessary entries and update settings to allow local development

--- codestar/settings.py
+++ codestar/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%7@9#ui%q-5g#+ai0v&ma0os3qcibabv-+g+$tyng902ws(wlz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.herokuapp.com']
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
