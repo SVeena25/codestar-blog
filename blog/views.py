ea217dd27715a6e6e95fb666481d026b563710ae
Veena S <veenasriramoju@gmail.com>
2025-12-26 12:57:54 +0000

    Enhance admin interface for Post model with Summernote integration, update settings for debugging, and add URL routing for Summernote. Update requirements for new dependencies.

--- blog/views.py
+++ blog/views.py
@@ -7,3 +7,8 @@ from .models import Post
 class PostList(generic.ListView):
    queryset = Post.objects.all()
    template_name = "blog/post_list.html"
+
+# django classes used to display list of objects
+detailview = generic.DetailView
+createview = generic.CreateView
+updateview = generic.UpdateView  
