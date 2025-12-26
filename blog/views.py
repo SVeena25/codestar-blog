e9f9b97eb326993897241b981afe37b280bcdb98
Veena S <veenasriramoju@gmail.com>
2025-12-26 19:37:32 +0000

    Add base template, index page, and style; update views and settings for improved structure

--- blog/views.py
+++ blog/views.py
@@ -6,9 +6,6 @@ from .models import Post
 # Create your views here.
 class PostList(generic.ListView):
    queryset = Post.objects.all()
-   template_name = "blog/post_list.html"
+   template_name = "blog/index.html"
+   paginate_by = 6
 
-# django classes used to display list of objects
-detailview = generic.DetailView
-createview = generic.CreateView
-updateview = generic.UpdateView  
